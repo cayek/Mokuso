2e3cb443e5f565c6469086b1b0d898832adaf25d
caye <kevin-ca@club-internet.fr>
2016-03-11 22:42:22 +0100

    start package

---
+++ R/blog.R
@@ -0,0 +1,15 @@
+#' 
+#' @export
+new_post <- function(title, serve = TRUE, dir = "_source",
+                     subdir = TRUE, skeleton_file = ".skeleton_post"){
+ 
+  # create Rmkdown with blog template if do not exist
+  
+  # open it in R studio
+  file.edit()
+  
+  # compile with jekkyl and display in viewer
+  
+  
+}
+
