94f1188d79c52a4e7819e0ea502d32beb30c998a
cayek <kevin-ca@club-internet.fr>
2016-03-13 09:06:30 +0100

    work on blog

--- R/blog.R
+++ R/blog.R
@@ -1,6 +1,6 @@
 #' 
 #' @export
-new_post <- function(title, serve = TRUE, dir = "_source",
+new_post <- function(title, serve = TRUE, dir = "~/Project/Notebook/Labnotebook",
                      subdir = TRUE, skeleton_file = ".skeleton_post"){
  
   # create Rmkdown with blog template if do not exist
@@ -10,6 +10,26 @@ new_post <- function(title, serve = TRUE, dir = "_source",
   
   # compile with jekkyl and display in viewer
   
+  
+}
+
+NormalizeName(file.name){
+  
+}
+
+
+BuildPost <- function(file.name,jekyll.dir = "~/Project/PersonnalWebsite"){
+  # build md with knitr
+  
+  # copy in place
+  
+  # build with jekyl 
+  
+  # view in rstudio viewer
+  
+}
+
+PublishPost <- function(){
   
 }
 
