dd85a81a21ad0cb4c4eb9234bbb76d611fa3e57b
cayek <kevin-ca@club-internet.fr>
2016-03-14 13:45:25 +0100

    work on blog.R

--- R/blog.R
+++ R/blog.R
@@ -1,25 +1,40 @@
 #' 
 #' @export
-new_post <- function(title, serve = TRUE, dir = "~/Project/Notebook/Labnotebook",
-                     subdir = TRUE, skeleton_file = ".skeleton_post"){
+NewLabnotebook <- function(title, labnotebook.dir = "~/Project/Notebook/Labnotebook",
+                     jekyll.dir = "~/Projects/PersonalWebSite/",
+                     skeleton.file = ".skeleton_post"){
  
+  
+  # create filename
+  file.name = paste0(labnotebook.dir,NormalizeName(title),".Rmd")
+  
   # create Rmkdown with blog template if do not exist
+  if(file.exists(file.name)) {
+    stop("This file exists. Remove it before.")
+  } else {
+    file.copy(skeleton.file,file.name)
+  }
   
   # open it in R studio
-  file.edit()
+  rstudioapi::navigateToFile(file.name)
   
   # compile with jekkyl and display in viewer
-  
-  
-}
-
-NormalizeName(file.name){
+  BuildPost(file.name)
   
 }
 
+NormalizeName <- function(title){
+  # name with date
+  return(paste0(format(Sys.time(), "%Y-%m-%d"),"-",sub("[ _./]","-",title)))
+}
 
-BuildPost <- function(file.name,jekyll.dir = "~/Project/PersonnalWebsite"){
-  # build md with knitr
+
+BuildPost <- function(file.name, jekyll.dir = "~/Projects/PersonalWebSite/"){
+
+  # build md in jekkyl _post/buildfromR dir and set image in  with knitr
+  # rmk : see render_jekkyl
+
+  # if filename is not well formated, generate a well formated md.
   
   # copy in place
   
@@ -29,7 +44,17 @@ BuildPost <- function(file.name,jekyll.dir = "~/Project/PersonnalWebsite"){
   
 }
 
-PublishPost <- function(){
+
+ServeLocalJekyll <- function(jekyll.dir = "~/Projects/PersonalWebSite/") {
+  # run jekkyl server
   
+  # onpen in viewer
+}
+
+DeployTIMCIMAGJekyll <- function(jekyll.dir = "~/Projects/PersonalWebSite/", intern = TRUE) {
+  
+  # run deploy script
+  script.file = paste0(jekyll.dir, "script/deploy.pl")
+  system(paste(script.file, ifelse(intern,"--interne")))
 }
 
