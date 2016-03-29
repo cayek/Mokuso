a276fd207953c4866b051fd3235932c72b60b586
cayek <cayek@timc-bcm-15.imag.fr>
2016-03-29 16:03:01 +0200

    r studio adin

---
+++ R/terminal.R
@@ -0,0 +1,77 @@
+
+SendToTerminal <- function(terminal.name) {
+  selected.code <- rstudioapi::getActiveDocumentContext()$selection[[1]]$text
+  # print(selected.code)
+  # escape char interpreted by bash
+  selected.code <- gsub("\\\\", "\\\\\\\\", selected.code)
+  selected.code <- gsub("\"", "\\\\\"", selected.code)
+  selected.code <- gsub("`", "\\\\`", selected.code)
+  selected.code <- gsub("\\$", "\\\\\\$", selected.code)
+  selected.code <- gsub("\\!", "\\\\\\!", selected.code)
+  # to escape \n by R
+  selected.code <- strsplit(selected.code, "\n")[[1]]
+  # print(selected.code)
+  for (s in selected.code) {
+    script <- paste0('WID=`xdotool search --name "',terminal.name,'" | head -1` ;',
+                     #'xdotool windowactivate --sync $WID;',
+                     'xdotool type --window $WID  "', s ,'" ;',
+                     'xdotool key --window $WID --delay 100 "Return" ;' )
+    # cat(script)
+    system(script)
+  }
+}
+
+
+#' open R on server
+#'
+#'
+#' @export
+OpenServer <- function(server.name = "patator") {
+  
+  workingProject <- rstudioapi::getActiveProject()
+  
+  system(paste0("xfce4-terminal --hold -e \"ssh " ,
+               Sys.info()[["user"]],"@",server.name,
+               " -t ",
+               "\\\"",
+               "cd ", workingProject,"; R",
+               "\\\" \"",
+               " -T \"",server.name," : ",workingProject,"\""))
+
+}
+
+#' open MRO
+#'
+#'
+#' @export
+OpenMRO <- function() {
+  
+  workingProject <- rstudioapi::getActiveProject()
+  
+  system(paste("xfce4-terminal --hold -e '" ,
+               ' bash -c " cd',workingProject,
+               ";",
+               '/usr/lib64/MRO-3.2.3/R-3.2.3/lib64/R/bin/R "',
+               "'",
+               "-T 'MRO : ",workingProject,"'"
+  ))
+  
+}
+
+#' send code to MRO
+#'
+#'
+#' @export
+SendToMRO <- function() {
+  terminal.name <- paste("MRO : ", workingProject)
+  SendToTerminal(terminal.name)
+}
+
+#' send code to patator
+#'
+#'
+#' @export
+SendToPatator <- function() {
+  terminal.name <- paste0("patator : ", workingProject)
+  SendToTerminal(terminal.name)
+}
