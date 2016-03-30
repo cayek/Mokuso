ecc67455e91a01e2d6a259d42c350f579d846fc4
cayek <cayek@timc-bcm-15.imag.fr>
2016-03-30 17:53:35 +0200

    bug in UpdateGithubPkgPackrat

--- R/packratEnv.R
+++ R/packratEnv.R
@@ -4,6 +4,8 @@
 #' @export
 UpdateGithubPkgPackrat <- function(pkgs = c("Mokusu", "TESS3enchoSen", "associationr", "BioCompToolsR", "tess3r")) {
   
+  changes <- FALSE
+  
   for (p in pkgs) {
     desc <- packageDescription(p)
     if (is.null(desc$GithubSHA1)) {
@@ -18,14 +20,17 @@ UpdateGithubPkgPackrat <- function(pkgs = c("Mokusu", "TESS3enchoSen", "associat
         message("Update : ", p)
         # install new version
         devtools::install_github(paste0(desc$GithubUsername,"/",desc$GithubRepo,"/",desc$GithubSubdir))
+        changes <- TRUE
       } else {
         message(p, " last github version")
       }
     }
   }
-  # packrate snapshot
-  packrat::snapshot()
-  
-  # push env 
-  system("git add packrat/packrat.lock; git commit -m \"Update packrat env: github package; git push")
+  if(changes) {
+    # packrate snapshot
+    packrat::snapshot()
+    
+    # push env 
+    system("git add packrat/packrat.lock; git commit -m \"Update packrat env: github package\"; git push")
+  }
 }
